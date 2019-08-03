3cb5d3464ba02683785ba587c3313bf344f7a66e
Jade Allen Cook <jadeallencook@gmail.com>
2019-08-02 21:56:21 -0700

    ⚙️ Configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { CocktailsComponent } from './pages/cocktails/cocktails.component';
+import { FoodComponent } from './pages/food/food.component';
+import { WineComponent } from './pages/wine/wine.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'wine', component: WineComponent },
+  { path: 'cocktails', component: CocktailsComponent },
+  { path: 'food', component: FoodComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
