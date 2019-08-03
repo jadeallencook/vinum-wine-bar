3df315e54d5f91833804d9d4d23776ca1b24614d
Jade Allen Cook <jadeallencook@gmail.com>
2019-08-02 21:56:08 -0700

    🛣 Generated new routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WineComponent } from './pages/wine/wine.component';
+import { FoodComponent } from './pages/food/food.component';
+import { CocktailsComponent } from './pages/cocktails/cocktails.component';
+import { MenuComponent } from './components/common/menu/menu.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WineComponent,
+    FoodComponent,
+    CocktailsComponent,
+    MenuComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
