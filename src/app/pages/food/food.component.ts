8ad6988b9209fced162fc5679c1fb1a544d62285
Jade Allen Cook <jadeallencook@gmail.com>
2019-08-05 23:17:59 -0700

    🏁 Completed food and drink menu

--- src/app/pages/food/food.component.ts
+++ src/app/pages/food/food.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import food from '../../../assets/food.json';
 
 @Component({
   selector: 'app-food',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food.component.scss']
 })
 export class FoodComponent implements OnInit {
-
-  constructor() { }
+  food = food;
+  constructor() {
+  }
 
   ngOnInit() {
   }
